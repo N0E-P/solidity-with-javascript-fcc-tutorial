5de5c827b454436f6cebfdb5acc18982eea52d98
N0E-P <pecci.noe@gmail.com>
2022-08-03 12:57:49 +0200

    first commit

---
+++ hardhat-simple-storage/hardhat.config.js
@@ -0,0 +1,46 @@
+require("@nomiclabs/hardhat-waffle")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+	const accounts = await hre.ethers.getSigners()
+
+	for (const account of accounts) {
+		console.log(account.address)
+	}
+})
+
+const RINKEBY_RPC_URL =
+	process.env.RINKEBY_RPC_URL || "https://eth-rinkeby/example"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
+module.exports = {
+	defaultNetwork: "hardhat",
+	networks: {
+		rinkeby: {
+			url: RINKEBY_RPC_URL,
+			accounts: [PRIVATE_KEY],
+			chainId: 4,
+		},
+		localhost: {
+			url: "http://127.0.0.1:8545/",
+			chainId: 31337,
+		},
+	},
+	solidity: "0.8.7",
+	etherscan: {
+		apiKey: ETHERSCAN_API_KEY,
+	},
+	gasReporter: {
+		enabled: true,
+		outputFile: "gas-report.txt",
+		noColors: true,
+		currency: "USD",
+		coinmarketcap: COINMARKETCAP_API_KEY,
+	},
+}
