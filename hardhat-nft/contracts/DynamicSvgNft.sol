810d133d89df43bbf150ec95f09e41fe2cd1c930
N0E-P <pecci.noe@gmail.com>
2022-08-03 17:10:10 +0200

    .

---
+++ hardhat-nft/contracts/DynamicSvgNft.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
+
+contract DynamicSvgNft is ERC721 {
+	uint256 private s_tokenCounter;
+	string private i_lowImageURI;
+	string private i_highImageUri;
+	string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64,";
+
+	constructor(string memory lowSvg, string memory highSvg)
+		ERC721("My beautiful svg nft", "mbsn")
+	{
+		s_tokenCounter = 0;
+	}
+
+	function svgToImageURI(string memory svg) public pure returns (string memory) {
+		string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+		return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
+	}
+
+	function mintNft() public {
+		_safeMint(msg.sender, s_tokenCounter);
+		s_tokenCounter = s_tokenCounter + 1;
+	}
+}
