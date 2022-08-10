01e93b926e7b09b60f6ed6ba49520fbc54178fb9
N0E-P <pecci.noe@gmail.com>
2022-08-10 19:44:37 +0200

    lil save

---
+++ hardhat-dao/hardhat.config.ts
@@ -0,0 +1,27 @@
+import "hardhat-deploy";
+import"@nomiclabs/hardhat-ethers";
+import"@typechain/hardhat";
+import {HardhatUserConfig} from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks:{
+    hardhat:{
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    },
+    localhost:{
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+
+    }
+  },
+  solidity: "0.8.8",
+  namedAccounts:{
+    deployer:{
+      default: 0,
+    },
+  },
+};
+
+export default config;
--- my-notes.md
+++ my-notes.md
@@ -1,6 +1,6 @@
 # Solidity avec Patrick
 
-Timecode : 1:03:55:00 → 1:06:26:00
+Timecode : 1:03:55:00 → 1:06:46:00
 Objectif : 1:06:00:00
 
 ⭐️ Course Contents ⭐
